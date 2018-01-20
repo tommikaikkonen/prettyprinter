7003d5167eaa789b527eca5e79b800c677c2fd0e
Tommi Kaikkonen <tommi.kaikkonen@aalto.fi>
2018-01-20 12:52:56 -0800

    Use sort_dict_keys by default in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
 import sys
 
+from prettyprinter import set_default_config
+
+# Need this to deterministically test on Python 3.5
+set_default_config(sort_dict_keys=True)
+
 
 def pytest_ignore_collect(path, config):
     if 'test_dataclasses' in str(path) and sys.version_info < (3, 7):
