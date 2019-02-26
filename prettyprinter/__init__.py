768fa6b094bb9b169e7a3f7410ef2b07a701c331
Tommi Kaikkonen <tommi.kaikkonen@aalto.fi>
2019-02-25 22:49:32 -0800

    Bump version: 0.14.0 → 0.15.0

--- prettyprinter/__init__.py
+++ prettyprinter/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Tommi Kaikkonen"""
 __email__ = 'kaikkonentommi@gmail.com'
-__version__ = '0.14.0'
+__version__ = '0.15.0'
 
 from io import StringIO
 from importlib import import_module
